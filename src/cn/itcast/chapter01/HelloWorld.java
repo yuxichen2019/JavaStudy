575889a0ff110814d724f48c8c7b59865d28a7ea
yuxichen2019 <707148142@qq.com>
2020-06-22 13:57:31 +0800

    restart

--- src/cn/itcast/chapter01/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package cn.itcast.chapter01;
-
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("这是我的第一个Java程序！！");
-    }
-}
--- src/cn/itcast/chapter02/Example12.java
+++
@@ -1,4 +0,0 @@
-package cn.itcast.chapter02;
-
-public class Example12 {
-}
